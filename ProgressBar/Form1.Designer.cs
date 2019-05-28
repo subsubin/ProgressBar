17c061258486263f21e533d3b8b6c11f5f719ca4
Subin Kim <s2017s24@e-mirim.hs.kr>
2019-05-28 11:39:50 +0900

    #1 ProgressBar 예제 폼 디자인

--- ProgressBar/Form1.Designer.cs
+++ ProgressBar/Form1.Designer.cs
@@ -29,12 +29,88 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(12, 12);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(369, 30);
+            this.pbStatus.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.DarkRed;
+            this.btnRun.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRun.ForeColor = System.Drawing.Color.White;
+            this.btnRun.Location = new System.Drawing.Point(12, 48);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(180, 81);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "실행";
+            this.btnRun.UseVisualStyleBackColor = false;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(198, 48);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(183, 36);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "멈춤";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(198, 90);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(183, 39);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "리셋";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.Location = new System.Drawing.Point(135, 21);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(41, 12);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "상태 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(393, 140);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.pbStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "상태진행";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer Timer;
     }
 }
 
